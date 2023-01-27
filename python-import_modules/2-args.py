549861823b970fa615d5259743abcd31090e7096
JimmyHernandez <jimoem24@gmail.com>
2023-01-27 19:42:41 -0400

    trying

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,14 @@ import sys
 if __name__ == "__main__":
     n = len(sys.argv)
 
-    if n <= 2:
-        print("{} argument:".format(n - 1))
-
-    else:
+    if n > 1:
         print("{} arguments:".format(n - 1))
+
+    elif n == 0:
+        print("{} argument.".format(n - 1))
+
+    else :
+        print("{} argument.".format(n - 1))
 
     for idx in range(1, n):
         print("{}: {}".format(idx, sys.argv[idx]))
