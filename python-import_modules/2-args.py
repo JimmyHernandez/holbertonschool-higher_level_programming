90bc420bef33f73c54fac8feec65310b3b2f26a4
JimmyHernandez <jimoem24@gmail.com>
2023-01-27 19:18:58 -0400

    first commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     n = len(sys.argv)
 
-    print("{} arguments:".format(n - 1))
+    print("{} argument:".format(n - 1))
 
     for idx in range(1, n):
         print("{}: {} ".format(idx, sys.argv[idx]))
