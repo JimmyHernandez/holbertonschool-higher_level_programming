059952e6de8a1965097f32fb24d33d5b4ff362cb
JimmyHernandez <jimoem24@gmail.com>
2023-01-27 19:58:31 -0400

    first commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if n >= 1:
         print("{} arguments:".format(n - 1))
     else:
-        print("{} arguments:".format(n - 1))
+        print("{} arguments.".format(n - 1))
 
     for idx in range(1, n):
         print("{}: {}".format(idx, sys.argv[idx]))
