b3b27ff680823ef76f90a5a607faca22b4172fc8
JimmyHernandez <jimoem24@gmail.com>
2023-02-14 17:19:10 -0400

    add shebang and docstring

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,2 +1,11 @@
+#!/usr/bin/python3
+""" The class Rectangle is a blueprint for creating objects that represent rectangles.
+    """
+
+
 class Rectangle:
+    """ `pass` is a null operation. When it is executed, nothing happens.
+    It is useful as a placeholderwhen a statement is required syntactically
+    , but no code needs to be executed. """
+
     pass
