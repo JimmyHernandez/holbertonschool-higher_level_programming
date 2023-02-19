a79e15c3b229fd13e053fa37cda7f175181ac9bf
JimmyHernandez <jimoem24@gmail.com>
2023-02-19 17:32:56 -0400

    first class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ Print_sorted prints the elements of the list
+"""
+
+
+def is_same_class(obj, a_class):
+
+    if obj is a_class:
+        return (True)
+
+    else:
+        return (False)
