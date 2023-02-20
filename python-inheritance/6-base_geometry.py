33ebce08533976734430670f6f6033ef123637fa
JimmyHernandez <jimoem24@gmail.com>
2023-02-20 18:24:48 -0400

    first commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry is a class that does nothing."""
+
+
+class BaseGeometry:
+    """ executed, you can use the `pass` statement."""
+
+    def area(self):
+
+        raise Exception("area() is not implemented")
