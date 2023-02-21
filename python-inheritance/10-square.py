be6d4f042469df04e51876b5bc9d2e361e23670e
JimmyHernandez <jimoem24@gmail.com>
2023-02-21 18:00:26 -0400

    first commit

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Square inherits from Rectangle,
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """__init__ method takes a size parameter, which is validated
+ by the integer_validator method from the parent class.
+    """
+
+    def __init__(self, size):
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+
+        return super().area()
