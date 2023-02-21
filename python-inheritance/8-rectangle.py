42ced1f0010a8335300eaf581f6ec912859fddd7
JimmyHernandez <jimoem24@gmail.com>
2023-02-21 16:56:46 -0400

    documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,13 +3,16 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 __init__ is a constructor that initializes the attributes of the class.
 """
-class Rectangle(BaseGeometry):
 
-    def __init__(self, width, height):
-        """
+
+class Rectangle(BaseGeometry):
+    """
         :param width: The width of the rectangle
         :param height: The height of the rectangle
         """
+
+    def __init__(self, width, height):
+
         self.integer_validator("width", width)
         self.integer_validator("heigth", height)
         self.__width = width
