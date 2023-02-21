d0e71622a6fdac7bea70a3eabf62c7eca011a64e
JimmyHernandez <jimoem24@gmail.com>
2023-02-21 17:13:27 -0400

    pep8

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-""" __init__ is a function that initializes
-    the attributes of the class Rectangle"""
+"""
+Class that inherits from BaseGeometry.
+"""
+
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """
-   :param width: the width of the rectangle
-   :param height: The height of the rectangle
+    Defines a rectangle from BaseGeometry Class.
     """
 
     def __init__(self, width, height):
 
         self.integer_validator("width", width)
-        self.integer_validator("heigth", height)
+        self.integer_validator("height", height)
         self.__width = width
-        self.__heigth = height
+        self.__height = height
 
     def area(self):
-
-        return self.__width * self.__heigth
+        return self.__width * self.__height
 
     def __str__(self):
-        """
-        Returns the printable string.
-        """
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__heigth)
+
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
