ec72fb5c74b58e4f05c7d3e53adeb60de3e4d127
JimmyHernandez <jimoem24@gmail.com>
2023-02-24 19:58:31 -0400

    first commit

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""_summary_ """
+import json
+
+
+def to_json_string(my_obj):
+    """ The above code is converting the Python
+            object my_obj into a JSON string."""
+    return json.dumps(my_obj)
