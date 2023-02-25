a7245ecf6c57d6dd5ce655168be4d94f8a477068
JimmyHernandez <jimoem24@gmail.com>
2023-02-24 20:02:19 -0400

    first commit

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""_summary_ """
+import json
+
+
+def from_json_string(my_str):
+    """
+    Converting the string into a python object.
+    """
+    return json.loads(my_str)
