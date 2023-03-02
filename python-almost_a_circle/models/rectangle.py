b4c0c3a4567ae661975dd2e19fed3fc56b0b0533
JimmyHernandez <jimoem24@gmail.com>
2023-03-02 18:58:08 -0400

    add validation

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -33,7 +33,7 @@ class Rectangle(Base):
 
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -48,7 +48,7 @@ class Rectangle(Base):
 
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -63,7 +63,7 @@ class Rectangle(Base):
 
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
@@ -78,6 +78,6 @@ class Rectangle(Base):
 
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
