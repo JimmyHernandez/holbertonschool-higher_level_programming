e5a98ed2af6e67fe4f7272b2926a183346f6d490
JimmyHernandez <jimoem24@gmail.com>
2023-04-18 18:29:50 -0400

    show two columns

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -2,5 +2,5 @@
 -- This script is to list all
 -- records in the second_table
 
-SELECT * FROM second_table ORDER BY score DESC;
-SELECT name, score FROM second_table;
+SELECT name, score FROM second_table ORDER BY score DESC;
+
