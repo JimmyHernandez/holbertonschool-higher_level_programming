25f3c4d104bd4568ad711c778f0039ed9b355d4e
JimmyHernandez <jimoem24@gmail.com>
2023-04-21 18:50:01 -0400

    Master

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- This script is for list all privileges.
+-- of a given database in Mysql.
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+This repository is for SQL more queries.
