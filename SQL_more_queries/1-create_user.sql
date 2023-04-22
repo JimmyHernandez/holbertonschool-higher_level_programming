6406b833cfacef2a02e9ec8a331eee04f6f692ac
JimmyHernandez <jimoem24@gmail.com>
2023-04-22 11:09:40 -0400

    NEW TRY

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- This script is for list all privileges.
 -- of a given database in Mysql.
-CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
