b6f7f949292a66435fa211623caeec5bad219898
JimmyHernandez <jimoem24@gmail.com>
2023-04-22 22:42:59 -0400

    S

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- creates the states table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
-       (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
-       	name varchar(256) NOT NULL);
-
+       (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY  UNIQUE ,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+	name varchar(256) NOT NULL);
