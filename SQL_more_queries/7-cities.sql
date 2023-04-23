ed44d227b67666181407b9a3762f24b8356fecc6
JimmyHernandez <jimoem24@gmail.com>
2023-04-22 22:41:47 -0400

    PRIMARY

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- creates the states table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities
-       (id INT NOT NULL AUTO_INCREMENT FOREING KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+       (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
        	name varchar(256) NOT NULL);
+
