72e5d5678b2e9b6e33d9a898dba3de32bb02d7d8
JimmyHernandez <jimoem24@gmail.com>
2023-04-22 22:30:32 -0400

    Second commit

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- creates the states table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-    id INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (id),
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY (id),
     name VARCHAR(256) NOT NULL
 );
