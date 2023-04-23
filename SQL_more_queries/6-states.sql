b5942a32034db6e7e2dfaa063e6d843cd639d389
JimmyHernandez <jimoem24@gmail.com>
2023-04-22 22:33:14 -0400

    again

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,5 @@
 -- creates the states table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY (id),
-    name VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+       (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY  UNIQUE ,
+       	name varchar(256) NOT NULL);
