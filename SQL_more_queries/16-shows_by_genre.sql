f300692d06c1adbb3ba9f2185e7bbcf272614072
JimmyHernandez <jimoem24@gmail.com>
2023-04-24 18:01:04 -0400

    again

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,13 +1,3 @@
--- Only Comedy
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = "Comedy"
-ORDER BY tv_shows.title;
-
 -- Lists shows by genre
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
