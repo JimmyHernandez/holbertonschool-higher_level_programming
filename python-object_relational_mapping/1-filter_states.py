7e20535518dd49356489691cd4ce64ae3d6b5b6b
JimmyHernandez <jimoem24@gmail.com>
2023-04-27 17:38:11 -0400

    Add COLLATE for case sensitive, fix comment

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,8 @@ if __name__ == '__main__':
                          db=sys.argv[3],
                          port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' COLLATE SQL_Latin1_General_CP1_CS_AS -- Case-sensitive collation ORDER BY id ASC;")
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' COLLATE SQL_Latin1_General_CP1_CS_AS collation ORDER BY id ASC;")
     states = cur.fetchall()
 
     for state in states:
