298f15f4663c32198190a7ef07a80242df2445f0
JimmyHernandez <jimoem24@gmail.com>
2023-04-30 11:40:27 -0400

    add code to file

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+Lists all cities from hbtn_0e_0_usa database.
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    """User input for logging to Mysql."""
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         user=user,
+                         passwd=password,
+                         db=dataBase,
+                         port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities WHERE ORDER BY id ASC;")
+
+    cities = cur.fetchall()
+
+    for cities in cities:
+        print(cities)
+
+    cur.close()
+    db.close()
