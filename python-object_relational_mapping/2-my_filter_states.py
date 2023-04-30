db4cd54fd664bff5fbbaecfbc3a38b044b8faecf
JimmyHernandez <jimoem24@gmail.com>
2023-04-30 11:13:39 -0400

    arr user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,11 +8,17 @@ import MySQLdb
 
 if __name__ == '__main__':
 
+    """User input for logging to Mysql."""
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    dataBase = sys.argv[3]
+    state = sys.argv[4]
+
     db = MySQLdb.connect(host="localhost",
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3],
-                         state=sys.argv[4],
+                         user=user,
+                         passwd=password,
+                         db=dataBase,
                          port=3306)
     cur = db.cursor()
     cur.execute(
