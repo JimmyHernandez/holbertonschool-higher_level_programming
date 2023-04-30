d197633aa7346fc0e3a323a0482425066d28008c
JimmyHernandez <jimoem24@gmail.com>
2023-04-30 10:57:43 -0400

    add new content

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Display all values in the states table of hbtn_0e_0_usa database.
+Lists all states with N names from
+hbtn_0e_0_usa database.
 """
 import sys
 import MySQLdb
@@ -8,15 +9,16 @@ import MySQLdb
 if __name__ == '__main__':
 
     db = MySQLdb.connect(host="localhost",
-                         port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3]
-                         )
+                         db=sys.argv[3],
+                         port=3306)
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
     states = cur.fetchall()
 
     for state in states:
-        print(state)
+        if state[1][0] == "N":
+            print(state)
+    cur.close()
+    db.close()
