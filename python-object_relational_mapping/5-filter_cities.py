d2ea85894f6703e28ecb884ad63386b25a7276d1
JimmyHernandez <jimoem24@gmail.com>
2023-05-02 17:44:36 -0400

    new way

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,29 +8,18 @@ import MySQLdb
 if __name__ == '__main__':
 
     """User input for logging to Mysql."""
-
-    user = sys.argv[1]
-    password = sys.argv[2]
-    dataBase = sys.argv[3]
-    state = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost",
-                         user=user,
-                         passwd=password,
-                         db=dataBase,
-                         port=3306)
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3])
 
     cur = db.cursor()
-
-    query = "SELECT cities.id, cities.name, states.name \
-            FROM cities JOIN states ON cities.state_id = states.id \
-            WHERE states.name = '{}';".format(state)
-    cur.execute(query,)
-
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4]))
     states = cur.fetchall()
 
-    for state in states:
-        print(state)
-
-    cur.close()
-    db.close()
+    print(", ".join([state[1] for state in states]))
