47acbc84ab550e922648c77fa9f41b73d0b852d9
JimmyHernandez <jimoem24@gmail.com>
2023-07-02 20:15:31 -0400

    Add more conditions

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,11 @@
 const [arg] = process.argv.slice(2);
 
 if (arg) {
-    console.log("Arguments found");
-} else {
+    console.log("Argument found");
+}
+if (arg) {
     console.log("No argument");
+}
+else {
+    console.log("Arguments found")
 }
