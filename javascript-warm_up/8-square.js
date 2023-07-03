7c641d569b4762f6d13844211c78a7e69ea145b7
JimmyHernandez <jimoem24@gmail.com>
2023-07-03 11:53:59 -0400

    first commit

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const process = require('process');
+const argv = process.argv;
+const int = parseInt(argv[2]);
+
+if (isNaN(int)) {
+    console.log('Missing size');
+} else {
+    for (let i = 0, s; i < argv[2]; i++) {
+        s = '';
+        for (let j = 0; j < argv[2]; j++) {
+            s += 'X';
+        }
+        console.log(s);
+    }
+}
