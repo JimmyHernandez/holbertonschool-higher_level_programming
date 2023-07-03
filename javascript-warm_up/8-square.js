b8e9e15e0246db70c100d933808f161cfd58013a
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2023-07-03 11:55:30 -0400

    Update 8-square.js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,13 +4,13 @@ const argv = process.argv;
 const int = parseInt(argv[2]);
 
 if (isNaN(int)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let i = 0, s; i < argv[2]; i++) {
-        s = '';
-        for (let j = 0; j < argv[2]; j++) {
-            s += 'X';
-        }
-        console.log(s);
+  for (let i = 0, s; i < argv[2]; i++) {
+    s = '';
+      for (let j = 0; j < argv[2]; j++) {
+        s += 'X';
+    }
+console.log(s);
     }
 }
