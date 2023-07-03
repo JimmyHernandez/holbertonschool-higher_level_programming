68ea108ab725951b062021ead38dbc7b8f5abfaa
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2023-07-03 10:59:02 -0400

    Update 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,9 +3,9 @@ const process = require('process');
 const argv = process.argv;
 
 if (isNaN(argv[2])) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else if (argv.length > 1) {
-    const number = argv[2];
-    const integer = parseInt(number);
-    console.log('My number: ' + integer);
+  const number = argv[2];
+  const integer = parseInt(number);
+  console.log('My number: ' + integer);
 }
