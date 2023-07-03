7c940d1c4ca075730aabe6e80022d7ac3e1aac34
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2023-07-03 15:46:31 -0400

    Update 11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,15 +5,15 @@ let i;
 const arr = [];
 
 for (i = 2; i < process.argv.length; i++) {
-    if (Number.isNaN(parseInt(process.argv[i])) === false) {
-        arr[i - 2] = parseInt(process.argv[i]);
-    }
+  if (Number.isNaN(parseInt(process.argv[i])) === false) {
+    arr[i - 2] = parseInt(process.argv[i]);
+  }
 }
 if (arr.length > 1) {
-    biggest = Math.max.apply(null, arr);
-    i = arr.indexOf(biggest);
-    arr[i] = -Infinity;
-    biggest = Math.max.apply(null, arr);
+  biggest = Math.max.apply(null, arr);
+  i = arr.indexOf(biggest);
+  arr[i] = -Infinity;
+  biggest = Math.max.apply(null, arr);
 }
 
 console.log(biggest);
