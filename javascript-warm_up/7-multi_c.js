dd3cefefe567a2c5892fc409fb947ad5bd9d2fe7
JimmyHernandez <jimoem24@gmail.com>
2023-07-03 11:36:18 -0400

    fix with semi standard

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 const process = require('process');
 const argv = process.argv;
-const len = argv.length;
 
 if (isNaN(argv[2])) {
     console.log('Missing number of occurrences');
-
 } else {
-
-    for (let i = 0; i < argv[2]; i++)
+    for (let i = 0; i < argv[2]; i++) {
         console.log('C is fun');
+    }
 }
-
-
