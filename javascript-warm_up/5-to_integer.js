378d464204152cc8bfaf769cdb4703eb1a901dfd
JimmyHernandez <jimoem24@gmail.com>
2023-07-03 10:58:35 -0400

    first commit

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const process = require('process');
+const argv = process.argv;
+
+if (isNaN(argv[2])) {
+    console.log('Not a number');
+} else if (argv.length > 1) {
+    const number = argv[2];
+    const integer = parseInt(number);
+    console.log('My number: ' + integer);
+}
