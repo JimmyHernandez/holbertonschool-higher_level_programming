4bff5e1d6497847fc2f1c60993283d2ae6702a07
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2023-07-05 11:40:01 -0400

    Update 3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -11,11 +11,11 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
-    s += 'X';
+  s += 'X';
           }
       console.log(s);
-  }
     }
+  }
 }
 
 module.exports = Rectangle;
