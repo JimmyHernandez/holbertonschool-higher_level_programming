9e9613f3aa9bf3c479d9a6295d51faf70311367c
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2023-07-05 11:37:10 -0400

    Update 3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -5,16 +5,17 @@ class Rectangle {
     if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
+    }
   }
-  }
+  
   print() {
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
-      s += 'X';
+    s += 'X';
           }
       console.log(s);
-        }
+  }
     }
 }
 
