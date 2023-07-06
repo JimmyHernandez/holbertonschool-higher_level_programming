9225e439aafe1869f694e37c8f9a4f8881cb30e8
Jimmy Hernandez Rivera <105442329+JimmyHernandez@users.noreply.github.com>
2023-07-06 13:01:46 -0400

    Update 7-occurrences.js

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let ocurrences = 0;
-    for (let i = 0; i < list.length; i++) {
-        if (searchElement === list[i]) {
-            ocurrences++;
+  let ocurrences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      ocurrences++;
         }
     }
     return ocurrences;
